a2eaeef9f7950fa0fd0d96c1c23092778e6df981
Virgiawan Listiyandi <listiyandiv@gmail.com>
2024-11-17 00:33:15 +0700

    feat(tools): tools management api

--- app/Models/Tool.php
+++ app/Models/Tool.php
@@ -2,9 +2,27 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
-class Tool extends Model
+class Tool extends BaseModel
 {
-    //
+    use SoftDeletes;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'tools';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'slug',
+        'image',
+    ];
 }
